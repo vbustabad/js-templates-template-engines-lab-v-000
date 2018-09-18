f33e191e7970d0a3ff4bbc1e56682194fd921320
Veronica Bustabad <vbustabad@gmail.com>
2018-09-18 22:36:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function postComment() {
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commentText").value;
+
+  var commentTemplate = document.getElementById("comment-template").innerHTML;
+
+  var templateFn = _.template(commentTemplate);
+
+  var commentsDiv = document.getElementById("comments");
+
+  var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
+
+  commentsDiv.innerHTML += templateHTML;
+}
+
+function createPost() {
+  var post_title = document.getElementById("postTitle").value;
+  var post_body = document.getElementById("postBody").value;
+  var post_author = document.getElementById("postAuthor").value;
+
+  var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+
+  var postDiv = document.getElementById("post");
+
+  var postTemplateHTML = postTemplate({ 'post_title': post_title, 'post_body': post_body, 'post_author': post_author });
+
+  postDiv.innerHTML += templateHTML;
+}
